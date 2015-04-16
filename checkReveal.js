9c8b00f519471b725d9c6c5bd5c3160e3c31757d
Brandon Flynn <btflynn@gmail.com>
2015-04-16 14:44:49 -0400

    adding js

---
+++ checkReveal.js
@@ -0,0 +1,36 @@
+
+  var revealCheckedBoxes = {
+    // Array of all the checked checkboxes
+    checkedBoxes: $('.channel-topic-dropdowns .topics input[type=checkbox]'),
+    
+    container: $('.parent'),
+    
+    // Returns true if checkbox is checked 
+    isChecked: function(target){
+    	return target.prop('checked');
+  	},
+    // Checks the container checkbox to open the container
+    openContainer: function($el){
+      var $container = $el.closest(this.container),
+        	$containerCheckbox = $container.find('input[type=checkbox]').eq(0);
+    	$containerCheckbox.prop('checked', true);
+    },
+    
+    init: function(){
+      // Reveal each checkbox currently checked, by opening the dropdown it's in. 
+      for(var i=0; i < this.checkedBoxes.length; i++){
+        $currentBox = $(this.checkedBoxes[i]);
+        if(this.isChecked($currentBox)){
+          this.openContainer($currentBox);
+        }
+      }
+    }
+  }
+  
+  $('button').on('click',function(){
+    revealCheckedBoxes.init();
+  });
+  
+
+
+
